f965ea51e33495f56add05f48f37846997fb32db
Meriam-elyns <meriamelyns@gmail.com>
2023-10-16 20:35:01 +0100

    s

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+'''Module for Rectangle unit tests.'''
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from random import randrange
+from contextlib import redirect_stdout
+import io
+
+
+class TestRectangle(unittest.TestCase):
+    '''Tests the Base class.'''
+
+    def setUp(self):
+        '''Imports module, instantiates class'''
+        Base._Base__nb_objects = 0
+
+    def tearDown(self):
+        '''Cleans up after each test_method.'''
+        pass
+
+    # ----------------- Tests for #2 ------------------------
+
+    def test_A_class(self):
+        '''Tests Rectangle class type.'''
+        self.assertEqual(str(Rectangle),
+                         "<class 'models.rectangle.Rectangle'>")
+
+    def test_B_inheritance(self):
+        '''Tests if Rectangle inherits Base.'''
+        self.assertTrue(issubclass(Rectangle, Base))
+
+    def test_C_constructor_no_args(self):
+        '''Tests constructor signature.'''
+        with self.assertRaises(TypeError) as e:
+            r = Rectangle()
+        s = "__init__() missing 2 required positional arguments: 'width' \
+and 'height'"
+        self.assertEqual(str(e.exception), s)
+
+    def test_C_constructor_many_args(self):
+        '''Tests constructor signature.'''
+        with self.assertRaises(TypeError) as e:
+            r = Rectangle(1, 2, 3, 4, 5, 6)
+        s = "__init__() takes from 3 to 6 positional arguments but 7 were \
+given"
+        self.assertEqual(str(e.exception), s)
+
+    def test_C_constructor_one_args(self):
+        '''Tests constructor signature.'''
+        with self.assertRaises(TypeError) as e:
+            r = Rectangle(1)
+        s = "__init__() missing 1 required positional argument: 'height'"
+
