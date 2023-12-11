cc56e859b142c550b7f5e5a136a322801b03f5dd
Meriam-elyns <meriamelyns@gmail.com>
2023-12-11 12:52:56 -0800

    s

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
-exports.addMeMaybe = function (number, theFunction) {
-  theFunction(++number);
-};
+// executes x times a function.
 
+exports.addMeMaybe = function (number, theFunction) {
+  number++;
+  theFunction(number);
+};
